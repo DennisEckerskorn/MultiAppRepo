e58518327e5aa9ca3b878a6a84cd0e8d4fbb4162
DennisEckerskorn <denniseckerskorn@gmail.com>
2024-11-26 20:53:26 +0100

    Frame works

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from ui.centered_window import CenteredWindow
 
 def main():
     # Crear una instancia de la ventana centrada
-    app = CenteredWindow(title="Ventana Principal", width_percentage=0.75, height_percentage=0.75)
+    app = CenteredWindow()
     
     # Ejecutar la ventana
     app.mainloop()
