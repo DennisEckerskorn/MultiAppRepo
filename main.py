ccae883f1701140e2ec844f658255363c719def8
DennisEckerskorn <denniseckerskorn@gmail.com>
2024-11-26 18:55:44 +0100

    Add

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from ui import CenteredWindow
+from MutiFunctionProgramProject.ui.centered_window import CenteredWindow
 
 def main():
     # Crear una instancia de la ventana centrada
-    app = CenteredWindow()
+    app = CenteredWindow(title="Ventana Principal", width_percentage=0.75, height_percentage=0.75)
     
     # Ejecutar la ventana
     app.mainloop()
